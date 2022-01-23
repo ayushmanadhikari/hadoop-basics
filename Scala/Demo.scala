68935815b66262a81be368c2515210ed9edc2711
ayushmanadhikari <unixparadoxfb@gmail.com>
2022-01-23 19:08:08 +0545

    class inheritence

--- Scala/Demo.scala
+++ Scala/Demo.scala
@@ -1,7 +1,7 @@
 import java.io._
 
 
-class Point(var xc: Int, var yc: Int){
+class Point(val xc: Int, val yc: Int){
     var x: Int = xc
     var y: Int = yc
 
@@ -15,12 +15,30 @@ class Point(var xc: Int, var yc: Int){
 
 }
 
+
+class Location(override val xc: Int, override val yc: Int, var zc: Int) extends Point(xc, yc){
+    var z: Int = zc
+
+    def move(dx: Int, dy: Int, dz: Int){
+        x = x + dx
+        y = y + dy
+        z = z + dz
+
+        println("New x: " + x)
+        println("New y: " + y)
+        println("New z: " + z) 
+
+    }
+}
+
+
 object Demo {
 
     def main(args: Array[String]){
-        val pt = new Point(10, 20)
+        var loc1 = new Location(10,20,30)
 
-        //move to new location
-        pt.move(10, 10)
+        //move the points from Location class
+        loc1.move(10,20,30)
+
     }
 }
